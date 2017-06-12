ab7819bb3f2f4f8bbcda13b162865e69e521144e
Alexander Leishman <leishman3@gmail.com>
2017-06-11 19:20:07 -0700

    update controller

--- app/controllers/api/gun_ranges_controller.rb
+++ app/controllers/api/gun_ranges_controller.rb
@@ -1,13 +1,17 @@
 class Api::GunRangesController < ApplicationController
 
   def index
-    shooter = User.where("name LIKE ?", "%#{params[:shooter]}%").first
-    @gun_ranges = []
-    if shooter
-      target = shooter.targets.last
-      if target
-        @gun_ranges = [target.gun_range]
+    if params[:shooter]
+      shooter = User.where("name LIKE ?", "%#{params[:shooter]}%").first
+      @gun_ranges = []
+      if shooter
+        target = shooter.targets.last
+        if target
+          @gun_ranges = [target.gun_range]
+        end
       end
+    else
+      @gun_ranges = GunRange.all
     end
   end
 end
